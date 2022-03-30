bcdfa469302495445ee0af324f9e3057a8d7b9a3
MatheusRoyal <engenharia9@royalciclo.com.br>
2022-03-30 14:06:24 -0300

    Adicionada funcionalidade de converter slddrw para pdf

--- lib/setores.py
+++ lib/setores.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import api_solidworks as sw
 
 def identificar(num):
     partes = num.split(".")
@@ -43,18 +44,44 @@ def achar_gabarito(gab, num):
     return achar_pdf(path, num)
 
 def achar_pdf(path, num):
-    try:
-        for file in os.listdir(path):
-            fileparts = file.split('.')
-            filename = ''.join(part + '.' for part in fileparts[:-1])
-            filename = filename[:-1]
-            try:
-                filename, version = filename.split('-')
-            except:
-                version = ''
-            if (file.endswith(".pdf") or file.endswith(".PDF")) and file.startswith(num):
-                path = path + file
-                break
-    except:
-        print("Pdf não encontrado")
+    #try:
+    version = -1
+    file_list = os.listdir(path)
+    for temp_file in file_list:
+        if not (temp_file.startswith(num) and (temp_file.endswith(".SLDDRW") or (temp_file.endswith(".pdf") or temp_file.endswith(".PDF")))):
+            continue
+        fileparts = temp_file.split('.')
+        temp_filename = ''.join(part + '.' for part in fileparts[:-1])
+        temp_filename = temp_filename[:-1]
+        try:
+            temp_version = temp_filename.split('-')[1]
+        except:
+            temp_version = "00"
+        if int(temp_version) > int(version):
+            filename = temp_filename
+            version = temp_version
+            file = temp_file
+            print(filename, version, file)
+    if (filename + ".pdf" in file_list):
+        path = path + filename + ".pdf"
+    elif (filename + ".PDF" in file_list):
+        path = path + filename + ".PDF"
+    elif (filename + ".SLDDRW" in file_list):
+        sw.gerar_pdf(path, filename + ".SLDDRW")
+        path = path + filename + ".pdf"
+
+    print(path)
+        #try:
+        #    filename, version = filename.split('-')
+        #except:
+        #    version = ''
+        #if file.endswith(".pdf") or file.endswith(".PDF"):
+        #    path = path + file
+        #    break
+    #except:
+    print("Pdf não encontrado")
     return path
+
+if __name__ == "__main__":
+    num = "33149.031"
+    identificar(num)
