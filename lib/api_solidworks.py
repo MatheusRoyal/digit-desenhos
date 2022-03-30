9f7591b31809ec4581c7e49e61a54b45c6bf45f2
MatheusRoyal <engenharia9@royalciclo.com.br>
2022-03-30 14:18:22 -0300

    Limpado código

--- lib/api_solidworks.py
+++ lib/api_solidworks.py
@@ -1,14 +1,6 @@
 import pySW
 
 def gerar_pdf(path, file):
-    #pathIn = 'N:\\Engenharia\\Desenhos\\Ferramentas\\Setor 01 - Conformação\\1001 - Conformadora de Eixo'
-
-    #partIn = '1001.015-07.SLDDRW'
-
-    #pathOut = 'N:\\Publico\\Manutenção\\Temp'
-
-    #partOut = '1001.015-07'
-
     sw = pySW.commSW()
     sw.connectToSW()
     sw.openDrw(path + "\\" + file)
